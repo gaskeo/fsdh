04dd4cc33f0744ae78092c52fb5435d2fdc3688b
gaskeo <zeytunyan2004@mail.ru>
2023-04-17 10:35:30 +0500

    feat(radio): use shared functions with checkbox

--- src/ui/inputs/radio/radio.ts
+++ src/ui/inputs/radio/radio.ts
@@ -1,11 +1,5 @@
-import ansiEscapes from "ansi-escapes";
-import {underline, whiteAndGreen} from "../../../utils/index.js";
-
-const exit = "\x03";
-const upButton = "\u001b[A";
-const downButton = "\u001b[B";
-const space = "\u0020";
-const enter = "\u000d";
+import {underline} from "../../../utils/index.js";
+import {after, buttons, clean, prepare, render} from "../shared.js";
 
 interface Radio {
     items: string[];
@@ -26,60 +20,32 @@ async function radio({
             const listener = (buffer: Buffer) => buttonHandler(buffer.toString());
 
             function buttonHandler(button: string) {
-                if (button === exit) {
-                    after();
+                if (button === buttons.exit) {
+                    after(listener);
                     return reject(new Error(ctrlCErrorMessage));
                 }
 
-                if ([upButton, "w"].includes(button)) {
+                if ([buttons.upButton, "w"].includes(button)) {
                     if (currentPosition > 0) {
                         currentPosition--;
                     } else return;
-                } else if ([downButton, "s"].includes(button) && currentPosition !== items.length - 1) {
+                } else if ([buttons.downButton, "s"].includes(button) && currentPosition !== items.length - 1) {
                     currentPosition++;
-                } else if (button === space) {
+                } else if (button === buttons.space) {
                     selectedItem = currentPosition;
-                } else if (button === enter) {
-                    after();
+                } else if (button === buttons.enter) {
+                    after(listener);
                     resolve(selectedItem);
                     return;
                 }
-                clean();
-                render();
+                clean(items.length + 1);
+                render(items, setSign);
             }
 
             function setSign(index: number, item: string) {
                 let output = selectedItem === index ? "☒ " : "☐ ";
                 output += index === currentPosition ? underline(item) : item;
                 return output;
-            }
-
-            function render() {
-                process.stdout.write((
-                    items.map((item, index) => setSign(index, item))).join("\n") + "\n");
-            }
-
-            function clean() {
-                process.stdout.write(ansiEscapes.eraseLines(items.length + 1));
-            }
-
-            function prepare() {
-                process.stdin.setRawMode(true);
-                process.stdin.resume();
-
-                title && process.stdout.write(title + "\n");
-                process.stdout.write(`${whiteAndGreen("↑ or W")} up\t` +
-                    `${whiteAndGreen("↓ or S")} down\t` +
-                    `${whiteAndGreen("Space")} toggle\t` +
-                    `${whiteAndGreen("Enter")} apply\n`);
-                process.stdout.write(ansiEscapes.cursorHide);
-                process.stdin.on("data", listener);
-            }
-
-            function after() {
-                process.stdin.setRawMode(false).resume();
-                process.stdout.write(ansiEscapes.cursorShow);
-                process.stdin.off("data", listener);
             }
 
             let currentPosition = defaultPosition || 0;
@@ -89,8 +55,8 @@ async function radio({
                 selectedItem = defaultSelected;
             } else selectedItem = 0;
 
-            prepare();
-            render();
+            prepare(title || "", listener);
+            render(items, setSign);
         }
     );
 }
