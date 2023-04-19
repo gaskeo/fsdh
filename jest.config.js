4e1561df0d69ee43e2ff7bbecaab5344f812ff81
gaskeo <zeytunyan2004@mail.ru>
2023-04-19 16:50:22 +0500

    tests(jest): edit jest config, add minimum coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,12 @@ export default {
   "collectCoverageFrom": [
     "src/**/*.ts"
   ],
+  coverageThreshold: {
+    global: {
+      functions: 80,
+      lines: 80,
+    },
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
   resolver: "ts-jest-resolver",
