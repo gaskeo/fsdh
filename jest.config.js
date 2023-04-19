544136eff361efe52f7dc0ebc1b63ac1ea326b18
gaskeo <zeytunyan2004@mail.ru>
2023-04-19 16:41:45 +0500

    tests(jest): add tests on package dataFormatting

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+    "src/**/*.ts"
+  ],
   preset: 'ts-jest',
   testEnvironment: 'node',
   resolver: "ts-jest-resolver",
