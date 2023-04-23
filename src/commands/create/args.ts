19fef7a968554af85463c10210d6c95bff3e555f
gaskeo <zeytunyan2004@mail.ru>
2023-04-23 14:26:29 +0500

    feat(create): remove unused branch in args handler

--- src/commands/create/args.ts
+++ src/commands/create/args.ts
@@ -18,40 +18,37 @@ export function getParams(): CreateArg {
         return {nameProvided: false, optionsProvided: false};
     }
 
-    if (args.length >= 4) {
-        const split = args[3].replace(/\\/g, "/").split("/");
-        const name = split[split.length - 1];
-        if (args.length === 4) {
-            return {
-                nameProvided: true,
-                name: {
-                    name: name,
-                    fullPath: args[3]
-                },
-                optionsProvided: false
-            };
-        } else {
-            const options: Option[] = [];
-            const optionsRaw = args[4];
-            if (optionsRaw.includes("m")) {
-                options.push("model");
-            }
-            if (optionsRaw.includes("sc")) options.push("styled-components");
-            else {
-                if (optionsRaw.includes("s")) options.push("scss");
-                else if (optionsRaw.includes("c")) options.push("css");
-            }
-
-            return {
-                nameProvided: true,
-                name: {
-                    name: name,
-                    fullPath: args[3]
-                },
-                optionsProvided: true,
-                options: options
-            };
+    const split = args[3].replace(/\\/g, "/").split("/");
+    const name = split[split.length - 1];
+    if (args.length === 4) {
+        return {
+            nameProvided: true,
+            name: {
+                name: name,
+                fullPath: args[3]
+            },
+            optionsProvided: false
+        };
+    } else {
+        const options: Option[] = [];
+        const optionsRaw = args[4];
+        if (optionsRaw.includes("m")) {
+            options.push("model");
         }
+        if (optionsRaw.includes("sc")) options.push("styled-components");
+        else {
+            if (optionsRaw.includes("s")) options.push("scss");
+            else if (optionsRaw.includes("c")) options.push("css");
+        }
+
+        return {
+            nameProvided: true,
+            name: {
+                name: name,
+                fullPath: args[3]
+            },
+            optionsProvided: true,
+            options: options
+        };
     }
-    return {nameProvided: false, optionsProvided: false};
 }
